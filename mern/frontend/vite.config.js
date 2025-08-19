d98c9925105f0d508c463232ba0e4d53057ec6e5
Chauhandevansh <chauhandevansh04@gmail.com>
2025-08-19 18:22:24 +0530

    Update vite.config.js

--- mern/frontend/vite.config.js
+++ mern/frontend/vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const API_URL = import.meta.env.VITE_API_URL;
+fetch(`${API_URL}/api/hello`)
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
