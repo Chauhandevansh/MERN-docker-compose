dd57caae33edc1e69df23d422cf1df4868d8411d
Chauhandevansh <chauhandevansh04@gmail.com>
2025-08-19 18:30:38 +0530

    Update vite.config.js

--- mern/frontend/vite.config.js
+++ mern/frontend/vite.config.js
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const API_URL = import.meta.env.VITE_API_URL;
-fetch(`${API_URL}/api/hello`)
+export default defineConfig(({ mode }) => {
+  // Load env variables based on the current mode
+  const env = loadEnv(mode, process.cwd(), '')
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+  return {
+    plugins: [react()],
+    define: {
+      'process.env': env,   // makes env variables available if needed
+    }
+  }
 })
