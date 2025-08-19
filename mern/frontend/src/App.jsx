052fda32cf4e300a0bf54436ea01d153c1f9800f
Chauhandevansh <chauhandevansh04@gmail.com>
2025-08-19 18:34:34 +0530

    Update App.jsx

--- mern/frontend/src/App.jsx
+++ mern/frontend/src/App.jsx
@@ -1,6 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
 
+const API_URL = import.meta.env.VITE_API_URL;
+
+fetch(`${API_URL}/api/hello`)
+  .then(res => res.json())
+  .then(data => console.log(data));
+
 const App = () => {
   return (
     <div className="w-full p-6">
